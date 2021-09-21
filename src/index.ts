eb09454cb4dc5d310d00596d73be7a6bf1bddcad
irg1008 <ivansudevlop@gmail.com>
2021-09-21 11:45:43 +0200

    Publishing to npm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 import CalendarHandler from "handler";
-import config from "config";
-
-const gcal = new CalendarHandler(config);
-
-(async () => {
-	const { data: events } = await gcal.getEvents({
-		query: "",
-	});
-	console.log(events);
-})();
+export default CalendarHandler;
