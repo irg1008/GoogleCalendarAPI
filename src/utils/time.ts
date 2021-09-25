d21bc174b0d5fc6b4807eec4c849c8b2d114302c
irg1008 <ivansudevlop@gmail.com>
2021-09-25 03:28:29 +0200

    Reverted to before

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,18 +1,3 @@
 const parseDate = (date: Date) => (date ? date.toISOString() : null);
 
-/**
- * Returns todays Date with T=23:59:59
- * @param days:number 0 by default, number of days to increase todays date.
- * @returns New Date object
- */
-const addDaysCeil = (days = 0) => {
-  const today = new Date();
-
-  today.setDate(today.getDate() + days);
-
-  const isoDate = `${today.toISOString().slice(0, 10)}T23:59:59.999Z`;
-
-  return new Date(isoDate);
-};
-
-export { parseDate, addDaysCeil };
+export { parseDate };
